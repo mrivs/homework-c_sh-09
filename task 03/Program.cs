7f3da2dce928ea886dc18ca4db9a640c9ded519e
mrivs <valentinilyins@gmail.com>
2022-11-14 01:06:11 +0300

    tasks added

--- task 03/Program.cs
+++ task 03/Program.cs
@@ -5,8 +5,18 @@ m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 /*/
 Console.WriteLine("Введите число m");
-int M = int.Parse(Console.ReadLine());
+int m = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Введите число n");
-int N = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
 
+if (n >= 0 && m >= 0) Console.WriteLine(GetAckerman(m, n));
+else Console.WriteLine("числа m и n должны быть неотрицательными");
+
+int GetAckerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return GetAckerman(m - 1, 1);
+    else if (m > 0 && n > 0) return GetAckerman(m - 1, GetAckerman(m, n - 1));
+    else return -1;
+}
